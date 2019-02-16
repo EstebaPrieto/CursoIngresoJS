2e0753beec68770051bca700ff8dc59cfc881e5c
EstebaPrieto <47401926+EstebaPrieto@users.noreply.github.com>
2019-02-16 20:06:10 -0300

    piedra,papel y tijera2

--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -6,26 +6,68 @@ var ContadorDePerdidas=0;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 3
-	 	
-
+	eleccionMaquina=Math.round(Math.random()*2)+1;	
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
-
+if (eleccionMaquina==1) {
+	alert("empató");
+	ContadorDeEmpates=ContadorDeEmpates+1;
+	document.getElementById("empatadas").value=ContadorDeEmpates;
+}	
+if (eleccionMaquina==2) {
+	alert("perdió");
+	ContadorDePerdidas=ContadorDePerdidas+1;
+	document.getElementById("perdidas").value=ContadorDePerdidas;
+}
+if (eleccionMaquina==3) {
+	alert("ganó");
+	ContadorDeGanadas=ContadorDeGanadas+1;
+	document.getElementById("ganadas").value=ContadorDeGanadas;
+}
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+if (eleccionMaquina==1) {
+	alert("ganó");
+	ContadorDeGanadas=ContadorDeGanadas+1;
+	document.getElementById("ganadas").value=ContadorDeGanadas;
+}	
+if (eleccionMaquina==2) {
+	alert("empató");
+	ContadorDeEmpates=ContadorDeEmpates+1;
+	document.getElementById("empatadas").value=ContadorDeEmpates;
+}
+if (eleccionMaquina==3) {
+	alert("perdió");
+	ContadorDePerdidas=ContadorDePerdidas+1;
+	document.getElementById("perdidas").value=ContadorDePerdidas;
+}	
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
-}//FIN DE LA FUNCIÓN
-
+if (eleccionMaquina==1) {
+	alert("perdió");
+	ContadorDePerdidas=ContadorDePerdidas+1;
+	document.getElementById("perdidas").value=ContadorDePerdidas;
+}	
+if (eleccionMaquina==2) {
+	alert("ganó");
+	ContadorDeGanadas=ContadorDeGanadas+1;
+	document.getElementById("ganadas").value=ContadorDeGanadas;
+}
+if (eleccionMaquina==3) {
+	alert("empató");
+	ContadorDeEmpates=ContadorDeEmpates+1;
+	document.getElementById("empatadas").value=ContadorDeEmpates;
+}			 	
+}
 function mostrarResultado()
-{
-
+{/*
+document.getElementById("ganadas").value=ContadorDeGanadas;
+document.getElementById("empatadas").value=ContadorDeEmpates;
+document.getElementById("perdidas").value=ContadorDePerdidas;*/
 }
