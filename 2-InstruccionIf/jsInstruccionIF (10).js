11586696b5528b8055550ef9bcce5e1783cb3040
EstebaPrieto <47401926+EstebaPrieto@users.noreply.github.com>
2019-02-16 20:15:42 -0300

    ejercicios IF 9y10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -3,9 +3,14 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	nota=Math.floor(Math.random() * 10)+1;	
-	if (nota) {
-		
+	if (nota==9||nota==10) {
+		alert("exceleten");
 	}
+	if (nota >=4) {
+		alert("aprobó");}
 	
+	if (nota<4) {
+		alert("vamos, la proxima se puede");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,6 +2,6 @@ var numero;
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-numero=Math.floor(Math.random() * 10);	
+numero=Math.floor(Math.random() * 10)+1;	
 alert(numero);
 }//FIN DE LA FUNCIÓN
