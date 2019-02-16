c5b2641bb242111876dfd1cd6d551a000b8eb34c
EstebaPrieto <47401926+EstebaPrieto@users.noreply.github.com>
2019-02-16 13:25:42 -0300

    ejercicio tp 3

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -11,33 +11,70 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 var precio=35;
 var total;
-var total_a;
-var total_b;
 var cant;
 var marca;
+var iibb;
+
  function CalcularPrecio () 
 {
-    /*parte A*/
-cant=document.getElementById("Cantidad").value;    
-if (cant>=6) {
-    total=precio*parseInt(cant);
-    total=total*0.5;
-} else {
-    total=precio*cant;
-}
-
-    /*fin parte A*/
-    /*parte B*/
-/*if (cant==5 && marca!="ArgentinaLuz") {
-    total=precio*parseInt(cant);
-    total=total*0.7;
-} else {
+    /*tomo los datos*/
+cant=document.getElementById("Cantidad").value;
+marca=document.getElementById("Marca").value;   
+    /*caso a*/
+if (cant<6) {
+ 
+    /*caso B*/
+if (cant==5 && marca =="ArgentinaLuz") {
     total=precio*parseInt(cant);
     total=total*0.6;
-}*/
+} else {
+    total=precio*parseInt(cant);
+    total=total*0.7;
+}
+    /*caso C*/
+  /*  C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” 
+  se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.*/
+if (cant==4) {
+   if (marca=="ArgentinaLuz"||marca=="FelipeLamparas") {
+    total=precio*parseInt(cant);
+    total=total*0.75; 
+   } else {
+    total=precio*parseInt(cant);
+    total=total*0.8;
+   } 
+}
+/*D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz" 
+ el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.*/
+if (cant==3) {
+    if (marca=="ArgentinaLuz") {
+    total=precio*parseInt(cant);
+    total=total*0.85;  
+    }
+    if (marca=="FelipeLamparas") {
+    total=precio*parseInt(cant);
+    total=total*0.9;    
+    }
+    if (marca=="JeLuz"||marca=="HazIluminacion"||marca=="Osram") {
+    total=precio*parseInt(cant);
+    total=total*0.95; 
+    }
+} 
+    /*fin caso a*/
+} else {
+    total=precio*parseInt(cant);
+    total=total*0.5;
+}
+/*E.	Si el importe final con descuento suma más de $120 
+ se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. */
+if (total>120) {
+    iibb=parseInt(total)*0.1;
+    
+    alert("IIBB: Usted pagó $"+iibb);
+}   
 
-
-document.getElementById("precioDescuento").value=total;     
+document.getElementById("precioDescuento").value=total;
+     
 } 
     
 
