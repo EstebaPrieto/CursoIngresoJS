77b20447e456c3e3e54405d031b072f8e8ad1c37
alumno <alumno>
2019-03-01 22:07:49 -0300

    recu ej 9sin terminar

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,31 @@
 function mostrar()
 {
+var marca;
+var l;
+var litros;
+var precio;
+var confirmar=true;
+var contadorPrecioPar=0;
+var marcaBarata;
+var litroBarato;
+var contadormenos50=0;
+var acumuladorPrecio=0;
+var promedio;
+var precioMax;
+var precioMin;
 
-}
+while (confirmar == true) {
+    marca = prompt("marca:");
+    
+    while (litros < 1 && litros > 3) {
+        l = prompt("litro");
+    litros = parseInt(l);
+        
+    }//fin validar litro
+
+
+}//fin confirmar true
+
+
+
+}//fin funcion
